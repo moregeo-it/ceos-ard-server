d4130f57c43dc3b35bbe11998428455319ae67b2
Saidy Barry <as.barry@moregeo.it>
2025-08-14 02:13:25 +0200

    Using mime types

--- app/utils/extraction.py
+++ app/utils/extraction.py
@@ -1,16 +1,5 @@
-import os
+import mimetypes
 
 
 def get_file_media_type(file_path):
-    _, file_extension = os.path.splitext(file_path)
-    media_types = {
-        ".yaml": "text/yaml",
-        ".yml": "text/yaml",
-        ".json": "application/json",
-        ".txt": "text/plain",
-        ".md": "text/markdown",
-        ".csv": "text/csv",
-        ".html": "text/html",
-        ".pdf": "application/pdf",
-    }
-    return media_types.get(file_extension, "text/plain")
+    return mimetypes.guess_type(file_path)[0] or "text/plain"
