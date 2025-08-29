eb55a5bfc0898ef652b4642f5582b9a9c8bf05fd
Saidy Barry <as.barry@moregeo.it>
2025-08-29 09:09:18 +0200

    Remove unecessary config on middleware.

--- app/main.py
+++ app/main.py
@@ -20,7 +20,6 @@ app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.CORS_ORIGINS],
-    allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
     allow_headers=["Authorization"],
 )
