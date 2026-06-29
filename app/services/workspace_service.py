d458defaa92e23f7b65bb1a4653eb6d47b68e0dd
Matthias Mohr <m.mohr@moregeo.it>
2026-06-29 12:37:01 +0200

    Fix encoding issue on Windows

--- app/services/workspace_service.py
+++ app/services/workspace_service.py
@@ -261,7 +261,7 @@ class WorkspaceService:
                     continue
 
                 try:
-                    yaml_content = pfs_document_path.read_text()
+                    yaml_content = pfs_document_path.read_text(encoding="utf-8")
                     validated_document = load(yaml_content, PFS_DOCUMENT(file=pfs_document_path.name, base_path=workspace.abs_path))
                     document_data = validated_document.data
 
@@ -313,7 +313,7 @@ class WorkspaceService:
 
                 documents_path = new_pfs_path / "document.yaml"
                 if documents_path.exists():
-                    yaml_content = documents_path.read_text()
+                    yaml_content = documents_path.read_text(encoding="utf-8")
                     validated_document = load(yaml_content)
                     documents_data = validated_document.data
                 else:
